5b2841cbc9a0ab2e896d6950d7423544649b3f83
cjh <18006009246@163.com>
2016-04-25 20:47:54 +0800

    gcc编程作业

---
+++ experiments/myproject/include/sum.h
@@ -0,0 +1,6 @@
+#ifndef _CALC_H
+#define _CALC_H
+
+int sum(int x, int y);//return sum of x to y
+
+#endif //_CALC_H
---
+++ experiments/myproject/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "include/sum.h"
+
+int main(void) {
+
+	printf("sum of 1 to 50 is %d\n",sum(1,50));
+
+	return 0;
+}
